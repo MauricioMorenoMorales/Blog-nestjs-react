ab6fdaaff31b264970a5a3f3127b88253b6e9162
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-10-02 10:42:07 -0500

    Starting expanding controller of login

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { UserModule } from './user/user.module';
       retryDelay: 3000,
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
